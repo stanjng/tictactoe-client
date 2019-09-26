583254cbc7d1520cacfb4628adeff92f112ba6b7
stanjng <sljadverts@gmail.com>
2019-09-26 11:12:29 -0400

    Created win function portion of game engine

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -45,73 +45,56 @@ const insertO = function () {
 }
 
 const win = function () {
-  if (gameBoard[0] === gameBoard[1] === gameBoard[2]) {
+  if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
     if (gameBoard[0] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[0] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else if (gameBoard[3] === gameBoard[4] === gameBoard[5]) {
+  } else if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
     if (gameBoard[3] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[3] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else if (gameBoard[6] === gameBoard[7] === gameBoard[8]) {
+  } else if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
     if (gameBoard[6] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[6] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else if (gameBoard[0] === gameBoard[3] === gameBoard[6]) {
+  } else if (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) {
     if (gameBoard[0] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[0] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else if (gameBoard[1] === gameBoard[4] === gameBoard[7]) {
+  } else if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
     if (gameBoard[1] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[1] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else if (gameBoard[2] === gameBoard[5] === gameBoard[8]) {
+  } else if (gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) {
     if (gameBoard[2] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[2] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else if (gameBoard[0] === gameBoard[4] === gameBoard[8]) {
+  } else if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
     if (gameBoard[0] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[0] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else if (gameBoard[2] === gameBoard[4] === gameBoard[6]) {
+  } else if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
     if (gameBoard[2] === 'X') {
       alert('Player X won!')
     } else if (gameBoard[2] === 'O') {
       alert('Player O won!')
-    } else {
-      alert('something happened')
     }
-  } else {
-    console.log('Keep it up!')
   }
+  console.log('Keep it up!')
 }
 
 module.exports = {
